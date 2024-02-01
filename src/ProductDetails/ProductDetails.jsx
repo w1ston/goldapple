58dfdda96c2e1e5acaf9bd06d5a01a8bda466ede
Valery <petrov.valerij0231@gmail.com>
2024-02-01 23:31:21 +0300

    refactor

--- src/ProductDetails/ProductDetails.jsx
+++ src/ProductDetails/ProductDetails.jsx
@@ -1,11 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate, useParams} from "react-router-dom";
+import './ProductDetails.css';
+import Footer from "../Components/Footer/Footer";
 
 const ProductDetails = () => {
     const {productName} = useParams();
     const formattedName = productName.replace(/-/g, ' ');
     const [productDetails, setProductDetails] = useState(null);
-
+    const [hasFetched, setHasFetched] = useState(false);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [activeShade, setActiveShade] = useState(null);
+    const [productPhotos, setProductPhotos] = useState([]);
+    const [shadePhotos, setShadePhotos] = useState([]);
     const navigate = useNavigate();
 
     const handleGoBack = () => {
@@ -13,11 +19,72 @@ const ProductDetails = () => {
     };
 
     useEffect(() => {
+        if (productDetails && productDetails.productPhotos) {
+            setProductPhotos([...productDetails.productPhotos]);
+            if (productDetails.productShades && productDetails.productShades.length > 0) {
+                const allShadePhotos = productDetails.productShades.reduce((accumulator, shade) => {
+                    if (shade.photos) {
+                        return accumulator.concat(shade.photos);
+                    }
+                    return accumulator;
+                }, []);
+                setShadePhotos(allShadePhotos);
+            }
+        }
+    }, [productDetails]);
+
+    const handleClick = (index) => {
+        setActiveIndex(index);
+        setActiveShade(null);
+    };
+
+    const handleShadeClick = (shade) => {
+        if (activeShade && activeShade.id_shade === shade.id_shade) {
+            setProductPhotos([...productDetails.productPhotos]);
+            setShadePhotos([...shade.photos]);
+            setActiveShade(null);
+        } else {
+            setActiveShade(shade);
+            if (shade && shade.photos) {
+                setProductPhotos([...shade.photos]);
+                setShadePhotos([...productDetails.productPhotos]);
+            }
+        }
+    };
+
+    const handlePhotoClick = (index) => {
+        if (activeShade) {
+            const updatedProductPhotos = [...shadePhotos];
+            const updatedShadePhotos = [...productPhotos];
+
+            [updatedProductPhotos[index], updatedShadePhotos[activeIndex]] = [
+                updatedShadePhotos[activeIndex],
+                updatedProductPhotos[index],
+            ];
+
+            setProductPhotos(updatedProductPhotos);
+            setShadePhotos(updatedShadePhotos);
+
+            setActiveIndex(index);
+        }
+    };
+
+
+    const fetchData = () => {
         fetch(`http://ggoldapple.com/DataBase/product/getProductById.php?productName=${formattedName}`)
             .then(response => response.json())
-            .then(data => setProductDetails(data))
+            .then(data => {
+                setProductDetails(data);
+                setHasFetched(true);
+            })
             .catch(error => console.error('Ошибка:', error));
-    }, [formattedName, productDetails]);
+    };
+
+    useEffect(() => {
+        if (!hasFetched) {
+            fetchData();
+        }
+    }, [formattedName, hasFetched]);
 
     if (!productDetails) {
         return (
@@ -29,51 +96,99 @@ const ProductDetails = () => {
 
     if (productDetails.length === 0) {
         return (
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '40em', flexDirection: 'column'}}>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '40em',
+                flexDirection: 'column'
+            }}>
                 <h1>not found</h1>
                 <button onClick={handleGoBack} className="buttonBack type1">назад</button>
             </div>
         );
     }
 
-    const ProductDetails = ({ productInfo, productPhotos, productShades }) => {
-        return (
-            <div>
-                <h2>{productInfo.name_product}</h2>
-                <p>{productInfo.description}</p>
-                <h3 style={{fontSize: '30px'}}>{'★'.repeat(productInfo.rating)}</h3>
-
-                <h3>Product Photos:</h3>
-                <div>
-                    {productPhotos.map((photo, index) => (
-                        <img key={index} src={photo} alt={`Product Photo ${index}`} />
-                    ))}
-                </div>
-
-                <h3>Shades:</h3>
-                {productShades.map((shade) => (
-                    <div key={shade.id_shade}>
-                        <h4>{shade.name_shade}</h4>
-                        <div>
-                            {shade.photos.map((photo, index) => (
-                                <img key={index} src={photo} alt={`Shade Photo ${index}`} />
-                            ))}
-                        </div>
-                    </div>
-                ))}
-            </div>
-        );
-    };
-
     return (
         <div style={{width: '100%'}}>
             <img style={{width: '100%', height: '200px', objectFit: 'cover'}} src="../../Photos/newBrands.jpeg"
                  alt="photo"/>
-            <ProductDetails
-                productInfo={productDetails.productInfo}
-                productPhotos={productDetails.productPhotos}
-                productShades={productDetails.productShades}
-            />
+            <div className="product_info">
+                <h5 style={{fontSize: '20px', textAlign: 'center'}}>{'★'.repeat(productDetails.productInfo.rating)}</h5>
+                <div style={{overflow: 'hidden', display: 'flex', flexDirection: 'column'}}>
+                    <h1 style={{fontSize: '70px', position: 'relative', top: '50px', right: '100px', zIndex: '99'}}>
+                        {productDetails.productInfo.name_product}
+                    </h1>
+                    <div style={{position: 'relative'}}>
+                        <div style={{ display: "flex", alignItems: 'center', gap: '50px'}}>
+                            <div>
+                                <img
+                                    src={productPhotos[activeIndex]}
+                                    alt={`Product Photo ${activeIndex}`}
+                                    style={{width: '800px'}}
+                                />
+
+                                <div style={{
+                                    position: 'absolute',
+                                    top: 100,
+                                    left: 15,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    gap: '20px'
+                                }}>
+                                    {productPhotos.map((photo, index) => (
+                                        <img
+                                            key={index}
+                                            src={photo}
+                                            alt={`Product Photo ${index}`}
+                                            style={{
+                                                width: '100px',
+                                                marginRight: '5px',
+                                                cursor: 'pointer',
+                                                border: index === activeIndex ? '1px solid black' : '1px solid black',
+                                                opacity: index === activeIndex ? 1 : 0.5,
+                                                transition: 'opacity 0.3s ease',
+                                            }}
+                                            onClick={() => handleClick(index)}
+                                            className="product_photo"
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+
+                            <div>
+                                {productDetails.productShades.map((shade) => (
+                                    <div key={shade.id_shade} onClick={() => handleShadeClick(shade)} style={{ display: "flex", flexDirection: 'column', gap: '10px', width: '100%'}}>
+                                        <h4 className="shades">оттенки:</h4>
+                                        <p style={{ textAlign: 'center'}}>{shade.name_shade}</p>
+                                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                                            {shadePhotos.slice(0, 1).map((photo, index) => (
+                                                <div key={index} onClick={() => handlePhotoClick(index)} className="shades_info">
+                                                    <img
+                                                        src={photo}
+                                                        alt={`Shade Photo ${index}`}
+                                                        style={{
+                                                            width: index === activeIndex && activeShade === shade ? '100px' : '100px',
+                                                            height: index === activeIndex && activeShade === shade ? '100px' : '100px',
+                                                            borderRadius: '50%',
+                                                            cursor: 'pointer',
+                                                            border: '1px solid black',
+                                                            marginRight: '5px',
+                                                        }}
+                                                    />
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer>
+                <Footer/>
+            </footer>
         </div>
     );
 };
